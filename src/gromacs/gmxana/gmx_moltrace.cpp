59e1c496d6f9b1657724da989cade26f3530ec59
Petter Johansson <pettjoha@kth.se>
2016-12-20 16:29:47 +0100

    Clean up gmx moltrace

--- src/gromacs/gmxana/gmx_moltrace.cpp
+++ src/gromacs/gmxana/gmx_moltrace.cpp
@@ -36,21 +36,17 @@
  */
 #include "gmxpre.h"
 
+#include <array>
 #include <cctype>
 #include <cmath>
 #include <cstdlib>
 #include <cstring>
-
-#include <array>
 #include <vector>
-#include <iostream>
 
 #include "gromacs/commandline/pargs.h"
-#include "gromacs/commandline/viewit.h"
 #include "gromacs/fileio/trxio.h"
 #include "gromacs/fileio/xvgr.h"
 #include "gromacs/gmxana/gmx_ana.h"
-#include "gromacs/gmxana/gstat.h"
 #include "gromacs/math/units.h"
 #include "gromacs/math/vec.h"
 #include "gromacs/pbcutil/pbc.h"
@@ -58,32 +54,50 @@
 #include "gromacs/topology/index.h"
 #include "gromacs/topology/topology.h"
 #include "gromacs/utility/arraysize.h"
-#include "gromacs/utility/cstringutil.h"
 #include "gromacs/utility/fatalerror.h"
 #include "gromacs/utility/futil.h"
-#include "gromacs/utility/gmxassert.h"
 #include "gromacs/utility/smalloc.h"
 
 using namespace std;
 
-using RVec = array<real, DIM>;
-using Frames = vector<vector<RVec>>;
+/****************************************************************************
+ * This program backtraces molecules from their final positions.            *
+ * Petter Johansson, Stockholm 2016                                         *
+ ****************************************************************************/
 
-struct RLim {
+using RVec   = array<real, DIM>;     // easiest to use proper arrays for positions
+using Frames = vector<vector<RVec>>; // each frame (one per time) needs a vector
+using Times  = vector<real>;         // making things slightly more readable
+                                     // (albeit obfuscating the object)
+
+struct FrameData {
+    Frames frames;
+    Times  times;
+};
+
+struct RLims {
+    RLims(t_pargs pa[], const int pasize, const rvec rmin_in, const rvec rmax_in)
+         :rmin_set{static_cast<bool>(opt2parg_bSet("-rmin", pasize, pa))},
+          rmax_set{static_cast<bool>(opt2parg_bSet("-rmax", pasize, pa))}
+         {
+             copy_rvec(rmin_in, rmin);
+             copy_rvec(rmax_in, rmax);
+         }
+
     bool rmin_set,
          rmax_set;
     rvec rmin,
          rmax;
 };
 
-Frames collect_positions(const char             *fn,
-                         int                    *grpindex,
-                         int                     grpsize,
-                         t_topology             *top,
-                         vector<real>           &times,
-                         struct RLim            &rlim,
-                         const int               ePBC,
-                         const gmx_output_env_t *oenv)
+static
+FrameData collect_positions(const char             *fn,
+                            int                    *grpindex,
+                            int                     grpsize,
+                            struct RLims           &rlim,
+                            const t_topology       *top,
+                            const int               ePBC,
+                            const gmx_output_env_t *oenv)
 {
     int          num_atoms;
     rvec        *x0;
@@ -143,6 +157,8 @@ Frames collect_positions(const char             *fn,
     }
 
     Frames xs_frames;
+    Times times;
+
     auto gpbc = gmx_rmpbc_init(&top->idef, ePBC, top->atoms.nr);
     do
     {
@@ -169,22 +185,24 @@ Frames collect_positions(const char             *fn,
 
     sfree(x0);
 
-    return xs_frames;
+    return FrameData { xs_frames, times };
 }
 
-Frames calc_relative_to_final(const Frames &xs_frames,
-                              vector<real> &times)
+static
+FrameData calc_relative_to_final(const FrameData &fdata)
 {
-    auto final_positions = xs_frames.back();
+    auto final_positions = fdata.frames.back();
     Frames xs_relative;
 
-    for (auto xs : xs_frames)
+    for (auto xs : fdata.frames)
     {
         const int num_atoms = xs.size();
         vector<RVec> xs_buf = xs;
 
         for (int i = 0; i < num_atoms; ++i)
         {
+            // The direction here is: final position -> current (in backtrace).
+            // Thus we subtract the final positions.
             for (int j = 0; j < DIM; ++j)
             {
                 xs_buf[i][j] -= final_positions[i][j];
@@ -194,37 +212,41 @@ Frames calc_relative_to_final(const Frames &xs_frames,
         xs_relative.push_back(xs_buf);
     }
 
-    const auto tend = times.back();
-    for (auto t_it = times.begin(); t_it != times.end(); ++t_it)
+    Times times_relative = fdata.times;
+    const auto tend = fdata.times.back();
+
+    for (auto &t : times_relative)
     {
-        *t_it -= tend;
+        t -= tend;
     }
 
-    return xs_relative;
+    return FrameData { xs_relative, times_relative };
 }
 
-void save_traces(const Frames           &xs_frames,
-                 const vector<real>     &times,
+static
+void save_traces(const FrameData        &fdata,
                  const char             *filename,
                  const gmx_output_env_t *oenv)
 {
-    const char *title = "Trace data per time from final positions";
+    const char *title  = "Trace data per time from final positions";
     const char *xlabel = "Time (ps)";
     const char *ylabel = "Positions (X0 Y0 Z0 X1 Y1 Z1 ... XN YN ZN for atom N)";
 
     auto file = xvgropen(filename, title, xlabel, ylabel, oenv);
+    auto t_it = fdata.times.cbegin();
+    auto frame_it = fdata.frames.cbegin();
 
-    // xvgr_legend(den, nr_grps, (const char**)grpname, oenv);
-
-    auto t = times.cbegin();
-    for (auto x_it = xs_frames.cbegin(); x_it != (xs_frames.cend()-1); ++x_it)
+    while (frame_it != (fdata.frames.cend()-1))
     {
-        fprintf(file, "%12.3f", *t++);
-        for (auto atom : *x_it)
+        fprintf(file, "%12.3f", *t_it);
+        for (auto atom : *frame_it)
         {
             fprintf(file, "  %8.3f  %8.3f  %8.3f", atom[XX], atom[YY], atom[ZZ]);
         }
         fprintf(file, "\n");
+
+        ++t_it;
+        ++frame_it;
     }
 
     xvgrclose(file);
@@ -234,22 +256,23 @@ int gmx_moltrace(int argc, char *argv[])
 {
     const char        *desc[] = {
         "[THISMODULE] backtraces molecules from their final position of a ",
-        "trajectory and calculates the difference from that position for every ",
-        "frame. In effect this will show from where a selected molecule ",
-        "arrived.",
+        "trajectory and calculates the difference from that position for ",
+        "every frame. In effect this will show from where a selected ",
+        "molecule arrived.",
         "[PAR]",
-        "The selection of molecules can be done through an index group and/or ",
-        "through submitting minimum and maximum coordinates for the final ",
-        "particle positions to include. This is done by supplying [TT]-rmin[tt] ",
-        "and [TT]-rmax[tt] vectors. For [TT]-rmax[tt] a value of -1.0 corresponds ",
-        "to using the full system length along that dimension. The time of the ",
-        "final frame is the final frame read by the trajectory: this is controlled ",
-        "through the flags [TT]-b[tt], [TT]-e[tt] and [TT]-dt[tt] described ",
-        "below.",
+        "The selection of molecules can be done through an index group ",
+        "and/or through submitting minimum and maximum coordinates for ",
+        "the final particle positions to include. This is done by supplying ",
+        "[TT]-rmin[tt] and [TT]-rmax[tt] vectors. For [TT]-rmax[tt] a value ",
+        "of -1.0 corresponds to using the full system length along that ",
+        "dimension. The time of the final frame is the final frame read ",
+        "by the trajectory: this is controlled through the flags [TT]-b[tt], ",
+        "[TT]-e[tt] and [TT]-dt[tt] described below.",
+        "",
     };
 
-    static rvec rmin = {0.0, 0.0, 0.0},
-                rmax = {0.0, 0.0, 0.0};
+    static rvec rmin = { 0.0,  0.0,  0.0},
+                rmax = {-1.0, -1.0, -1.0};
 
     t_pargs            pa[] = {
         { "-rmin", FALSE, etRVEC, { rmin },
@@ -257,6 +280,7 @@ int gmx_moltrace(int argc, char *argv[])
         { "-rmax", FALSE, etRVEC, { rmax },
           "Maximum coordinate values for final atom positions to include." },
     };
+
     const char        *bugs[] = {
     };
 
@@ -278,7 +302,7 @@ int gmx_moltrace(int argc, char *argv[])
     }
 
     int ePBC;
-    auto top = read_top(ftp2fn(efTPR, NFILE, fnm), &ePBC);
+    const auto top = read_top(ftp2fn(efTPR, NFILE, fnm), &ePBC);
 
     char **grpnames;
     int   *grpsizes;
@@ -288,43 +312,22 @@ int gmx_moltrace(int argc, char *argv[])
     snew(grpnames, 1);
     snew(grpsizes, 1);
 
-
     fprintf(stderr, "\nSelect group to collect traces for:\n");
     get_index(&top->atoms, ftp2fn_null(efNDX, NFILE, fnm),
               1, grpsizes, index, grpnames);
 
-    struct RLim rlim;
-    if (opt2parg_bSet("-rmin", asize(pa), pa))
-    {
-        rlim.rmin_set = true;
-        for (int i = 0; i < DIM; ++i)
-        {
-            rlim.rmin[i] = rmin[i];
-        }
-    }
-    else
-    {
-        rlim.rmin_set = false;
-    }
-    if (opt2parg_bSet("-rmax", asize(pa), pa))
-    {
-        rlim.rmax_set = true;
-        for (int i = 0; i < DIM; ++i)
-        {
-            rlim.rmax[i] = rmax[i];
-        }
-    }
-    else
-    {
-        rlim.rmax_set = false;
-    }
-    vector<real> times;
-    const auto xs_frames = collect_positions(ftp2fn(efTRX, NFILE, fnm),
-                                             *index, *grpsizes, top,
-                                             times, rlim, ePBC, oenv);
-    const auto xs_relative = calc_relative_to_final(xs_frames, times);
+    struct RLims rlim {pa, asize(pa), rmin, rmax};
 
-    save_traces(xs_relative, times, opt2fn("-o", NFILE, fnm), oenv);
+    const auto fdata = collect_positions(ftp2fn(efTRX, NFILE, fnm),
+                                         *index, *grpsizes, rlim,
+                                         top, ePBC, oenv);
+    const auto fdata_relative = calc_relative_to_final(fdata);
+
+    save_traces(fdata_relative, opt2fn("-o", NFILE, fnm), oenv);
+
+    sfree(index);
+    sfree(grpnames);
+    sfree(grpsizes);
 
     return 0;
 }
