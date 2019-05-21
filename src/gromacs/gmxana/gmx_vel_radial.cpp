9d38b5dab61e32e5d59f5fe3aaee98342d6e9285
Petter Johansson <pettjoha@kth.se>
2019-05-21 14:37:45 +0200

    radial: Can now sample either mass or velocity

--- src/gromacs/gmxana/gmx_vel_radial.cpp
+++ src/gromacs/gmxana/gmx_vel_radial.cpp
@@ -82,6 +82,12 @@
 #define EPRINTLN(X);
 #endif
 
+// Sample parameters
+enum class Parameter {
+    Velocity,
+    Mass
+};
+
 // Grid is ordered as: grid[iz][ir]
 template <typename T>
 using Grid = std::vector<std::vector<T>>;
@@ -120,7 +126,6 @@ static ComponentGrid calc_radial(const t_topology       *top,
         gmx_fatal(FARGS, "No velocities in trajectory");
     }
 
-    // const int natom = read_first_x(oenv, &status, fn, &t, &x, box);
     const auto gpbc = gmx_rmpbc_init(&top->idef, ePBC, frame.natoms);
 
     int e1, e2;
@@ -208,7 +213,7 @@ static ComponentGrid calc_radial(const t_topology       *top,
 
                 grid[ih][ir][0] += m * v0[axis];
                 grid[ih][ir][1] += m * rcomponent;
-                mass[ih][ir] += static_cast<double>(m);
+                mass[ih][ir] += m;
             }
         }
 
@@ -236,6 +241,166 @@ static ComponentGrid calc_radial(const t_topology       *top,
     close_trx(status);
 
     return grid;
+}
+
+// Calculate the radial components (vz, vr) of the dipole moment
+// in a grid of input spacing. vr is positive along the increasing
+// radial axis.
+static Grid<double> calc_radial_mass(const t_topology       *top,
+                                     int                     ePBC,
+                                     const char             *fn,
+                                     const int               axis,
+                                     const rvec              origin,
+                                     const bool              bUseOrigin,
+                                     const real              spacing,
+                                     int                    *gnx,
+                                     int                    *grpindex[],
+                                     const gmx_output_env_t *oenv)
+{
+    const auto atom = top->atoms.atom;
+
+    t_pbc          pbc;
+    t_trxframe    frame;
+    t_trxstatus   *status;
+
+    if (!read_first_frame(oenv, &status, fn, &frame, TRX_NEED_X))
+    {
+        gmx_fatal(FARGS, "Could not read trajectory");
+    }
+
+    const auto gpbc = gmx_rmpbc_init(&top->idef, ePBC, frame.natoms);
+
+    int e1, e2;
+    switch (axis)
+    {
+        case XX:
+            e1 = YY;
+            e2 = ZZ;
+            break;
+        case YY:
+            e1 = XX;
+            e2 = ZZ;
+            break;
+        case ZZ:
+            e1 = XX;
+            e2 = YY;
+            break;
+    }
+
+    /* Initialize 2D grid for radial distribution */
+    const auto box = frame.box;
+    const real rmax =
+        box[e1][e1] > box[e2][e2] ? 0.5 * box[e2][e2] : 0.5 * box[e1][e1];
+    const real hmax = box[axis][axis];
+
+    const auto nr = static_cast<size_t>(ceil(rmax / spacing));
+    const auto nh = static_cast<size_t>(ceil(hmax / spacing));
+
+    /* Grid is ordered as: grid[iz][ir]
+       Contains: mass (or number) values
+       counts of additions are stored separately for averaging */
+    Grid<double> mass(nh, std::vector<double> (nr, 0.0));
+    Grid<size_t> counts(nh, std::vector<size_t> (nr, 0));
+
+    /* Start while loop over frames */
+    gmx_bool bCont;
+    int      timecheck = 0;
+    rvec     dx, r0;
+
+    if (bUseOrigin)
+    {
+        r0[XX] = origin[XX];
+        r0[YY] = origin[YY];
+        r0[ZZ] = origin[ZZ];
+    }
+
+    const auto bHaveMass = static_cast<bool>(top->atoms.haveMass);
+
+    do
+    {
+        auto& x = frame.x;
+
+        set_pbc(&pbc, ePBC, box);
+        gmx_rmpbc(gpbc, frame.natoms, box, x); // Makes molecules whole
+
+        if (!bUseOrigin)
+        {
+            r0[XX] = 0.5 * box[XX][XX];
+            r0[YY] = 0.5 * box[YY][YY];
+            r0[ZZ] = 0.5 * box[ZZ][ZZ];
+        }
+
+        for (int n = 0; n < gnx[0]; n++)
+        {
+            const auto i = grpindex[0][n];
+            const auto& x0 = x[i];
+            pbc_dx(&pbc, r0, x0, dx);
+
+            const real r = sqrt(std::pow(dx[e1], 2) + std::pow(dx[e2], 2));
+            const real h = x0[axis];
+
+            if ((r < rmax) && (h < hmax))
+            {
+                const auto ir = static_cast<size_t>(floor(r / spacing));
+                const auto ih = static_cast<size_t>(floor(h / spacing));
+
+                const auto m = bHaveMass ? atom[i].m : 1.0;
+                mass[ih][ir] += static_cast<double>(m);
+                counts[ih][ir] += 1;
+            }
+        }
+
+        bCont = read_next_frame(oenv, status, &frame);
+        timecheck = check_times(frame.time);
+    }
+    while (bCont && (timecheck == 0));
+
+    for (size_t ih = 0; ih < nh; ++ih)
+    {
+        for (size_t ir = 0; ir < nr; ++ir)
+        {
+            const auto count = counts[ih][ir];
+
+            if (count > 0)
+            {
+                mass[ih][ir] /= static_cast<double>(count);
+            }
+        }
+    }
+
+    gmx_rmpbc_done(gpbc);
+    close_trx(status);
+
+    return mass;
+}
+
+template <typename T>
+static void print_grid_singles(const Grid<T> &grid,
+                               const char    *fn,
+                               const real     spacing)
+{
+    FILE *fp = gmx_ffopen(fn, "w");
+
+    fprintf(fp, "# %6s %8s %12s\n", "r (nm)", "z (nm)", "mass (u)");
+
+    real h = 0.5 * spacing;
+
+    for (const auto column : grid)
+    {
+        real r = 0.5 * spacing;
+
+        for (const auto value : column)
+        {
+            fprintf(fp, "%8.3f %8.3f %12.6f\n", r, h, value);
+            r += spacing;
+        }
+
+        h += spacing;
+    }
+
+    gmx_ffclose(fp);
+
+    return;
 }
 
 static void print_grid_components(const ComponentGrid &grid,
@@ -269,29 +434,39 @@ static void print_grid_components(const ComponentGrid &grid,
 int gmx_radial(int argc, char *argv[])
 {
     const char       *desc[] = {
-        "[THISMODULE] computes the radial density distribution of velocity ",
+        "[THISMODULE] computes the radial density distribution of samples ",
         "in a simulation.",
         "[PAR]",
         "The distribution is calculated on a 2D grid along the radius and height from",
         "a center axis. The position of this axis is by default the center of the",
         "simulation box. It can be given manually using the [TT]-origin[tt] argument.",
-        "The grid spacing is controlled using the [TT]-spacing[tt] argument.",
-        "[PAR]",
-        "The velocity components are written to the [TT]-o[tt] filename in (nm/ps).",
-        "The output is a mass average of the flow, if masses exist in the topology."
+        "The grid spacing is controlled using the [TT]-spacing[tt] argument."
     };
     const char       *axtitle    = "Z";
     real              spacing    = 0.1;
     rvec              origin     = {-1.0, -1.0, -1.0};
     gmx_output_env_t *oenv;
+
+    enum SampleSelection 
+    {
+        eSampleSel,
+        eVelocity,
+        eMass,
+        eSampleN
+    };
+    const char *sample_opt[eSampleN + 1] = { nullptr, "velocity", "mass", nullptr };
+
     t_pargs           pa[] = {
         { "-axis",     FALSE, etSTR, {&axtitle},
           "Set cylinder axis along X, Y or Z" },
         { "-origin",   FALSE, etRVEC, {&origin},
           "Origin of cylinder, only radial component is used" },
         { "-spacing",  FALSE, etREAL, {&spacing},
-          "Spacing of radial distribution function (nm)" }
+          "Spacing of radial distribution function (nm)" },
+        { "-sample",   FALSE, etENUM, {&sample_opt},
+          "Parameter to sample" }
     };
+
     int              *gnx;
     int             **grpindex;
     char            **grpname = nullptr;
@@ -334,6 +509,17 @@ int gmx_radial(int argc, char *argv[])
             gmx_fatal(FARGS, "Invalid axis '%s': must be X, Y or Z", axtitle);
     }
 
+    // Parse sample parameter
+    auto param = Parameter::Velocity;
+    switch (nenum(sample_opt))
+    {
+        case eVelocity:
+            break;
+        case eMass:
+            param = Parameter::Mass;
+            break;
+    }
+
     const auto bUseOrigin = static_cast<bool>(opt2parg_bSet("-origin", npargs, pa));
 
     snew(top, 1);
@@ -348,15 +534,35 @@ int gmx_radial(int argc, char *argv[])
     get_index(&top->atoms, ftp2fn_null(efNDX, NFILE, fnm),
               1, gnx, grpindex, grpname);
 
-    const auto velocity_components = calc_radial(
-        top, ePBC, ftp2fn(efTRX, NFILE, fnm), axis, origin, bUseOrigin, spacing,
-        gnx, grpindex, oenv);
+    switch (param)
+    {
+        case Parameter::Velocity:
+        {
 
-    print_grid_components(
-        velocity_components,
-        opt2fn("-o", NFILE, fnm),
-        spacing
-    );
+            const auto velocity_components = calc_radial(
+                top, ePBC, ftp2fn(efTRX, NFILE, fnm), axis, origin, bUseOrigin, spacing,
+                gnx, grpindex, oenv);
+            print_grid_components(
+                velocity_components,
+                opt2fn("-o", NFILE, fnm),
+                spacing
+            );
+            break;
+        }
+        
+        case Parameter::Mass:
+        {
+            const auto mass_components = calc_radial_mass(
+                top, ePBC, ftp2fn(efTRX, NFILE, fnm), axis, origin, bUseOrigin, spacing,
+                gnx, grpindex, oenv);
+            print_grid_singles(
+                mass_components,
+                opt2fn("-o", NFILE, fnm),
+                spacing
+            );
+            break;
+        }
+    }
 
     return 0;
 }
